6b12e5390e74d12f5b14717bc1464cabef7ff408
augustospr <augusto_spr@hotmail.com>
2023-11-27 17:48:50 -0300

    feito o login e trabalhando na adicao do produto no carrinho

--- src/components/product-item/index.jsx
+++ src/components/product-item/index.jsx
@@ -1,7 +1,9 @@
 import { BsCartPlus } from "react-icons/bs";
+import { useDispatch } from "react-redux";
 
 // Components
 import CustomButton from "../custom-button/index";
+import { addProductToCart } from "../../redux/cart/actions";
 
 // Styles
 import * as Styles from "./styles";
@@ -9,10 +11,16 @@ import * as Styles from "./styles";
 // Utilities
 
 const ProductItem = ({ product }) => {
+  const dispatch = useDispatch();
+
+  const handleProductClick = () => {
+    dispatch(addProductToCart(product));
+  }
+
   return (
     <Styles.ProductContainer>
       <Styles.ProductImage imageUrl={product.imageUrl}>
-        <CustomButton startIcon={<BsCartPlus />}>
+        <CustomButton startIcon={<BsCartPlus />} onClick={handleProductClick}>
           Adicionar ao carrinho
         </CustomButton>
       </Styles.ProductImage>
